016fc3f35c74dd7fe3151adda031e4dc5967337c
Newbie4dev <ryolyu4dev@gmail.com>
2023-11-07 17:04:21 +0300

    parcel add

---
+++ firstProjectByVideo/src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+	{ type: 'title', value: 'Hello World' },
+	{ type: 'text', value: 'some text description' },
+	{ type: 'columns', value: [
+      '11111111111',
+      '22222222222',
+      '33333333333'
+  ] },
+  {type: 'image', value: './src/assets/image.webp' },
+]
